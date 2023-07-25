0bb9e4cd9f5bc58b48478d614c911f3bfb7500b7
Marios <xrteach@gmail.com>
2023-07-25 15:32:46 +0300

    feat: Added Server to Cred object fix: Default server includes http

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,8 +1,9 @@
 import { writable } from 'svelte/store';
 
-export const defaultServer = writable('localhost:8080');
+export const defaultServer = writable('http://localhost:8080');
 export const logged = writable(false);
 export const cred = writable({
 	user: '',
-	token: ''
+	token: '',
+	server: ''
 });
