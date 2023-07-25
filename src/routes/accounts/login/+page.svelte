288a19977140db23f96511cbf22453e0f51aa01a
Marios <xrteach@gmail.com>
2023-07-25 16:42:24 +0300

    feat: If already logged in, log out

--- src/routes/accounts/login/+page.svelte
+++ src/routes/accounts/login/+page.svelte
@@ -34,6 +34,9 @@
 				goto('/home');
 			} else {
 				errorMessage = req.msg;
+				if (errorMessage == 'Already logged in') {
+					goto('/accounts/logout');
+				}
 			}
 		} else {
 			errorMessage = 'You must fill out all fields!';
